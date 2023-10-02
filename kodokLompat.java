7dc6547d6379881835931ff02bd0481a3d433edd
Giant77 <willyarsyad77@gmail.com>
2023-10-02 15:16:38 +0700

    Menambah fitur pergerakan, update skor dan posisi saat ini

--- kodokLompat.java
+++ kodokLompat.java
@@ -11,33 +11,98 @@ public class kodokLompat
     int[] coin = new int[boxSize];
     int[] mons = new int[boxSize];
 
-    public kodokLompat(String difficulty) {
+    public kodokLompat(int difficulty) {
         int monsCount = 3;
         int coinCount = 2;
 
         for (int i = 0, tmp; i < monsCount; i++) {
             tmp = random.nextInt(boxSize - 0);
             mons[tmp]++;
-            System.out.println(i + ": " + tmp);
+            // System.out.println(i + ": " + tmp);
         }
 
-        System.out.println("\ncoin:");
+        // System.out.println("\ncoin:");
         for (int i = 0, tmp; i < coinCount; i++) {
             tmp = random.nextInt(boxSize - 0);
             coin[tmp]++;
-            System.out.println(i + ": " + tmp);
+            // System.out.println(i + ": " + tmp);
         }
 
     }
 
+    static void movement(int i){
+        System.out.println("\nSekarang anda berada pada kotak ke-"+ (i+1));
+        System.out.println("Kemana anda akan melompat? (Angka)");
+        System.out.println("1. Lompat 1 kotak\n2. Lompat 2 kotak");
+        System.out.println("3. Lompat mundur 1 kotak\n4. Lompat mundur 2 kotak");
+    }
+
+
     public static void main(String[] args) {
-        kodokLompat k = new kodokLompat(null);
+        Scanner in = new Scanner(System.in);
+        int diff;
+        int i = 0;
+        int lompat;
+
         
-        System.out.println("arr:");
+        System.out.println("Selamat datang pada permainan Lompat hai katak, lompat!\n");
+        System.out.println("Silahkan pilih tingkat kesulitan (Angka)");
+        System.out.println("1. Easy\n2. Normal\n3. Hard\n4. Hardcore\n");
+
+        diff = in.nextInt();
+        kodokLompat k = new kodokLompat(diff);
         
-        for (int i = 0; i < k.boxSize; i++) {
-            System.out.println(i + ": " + k.mons[i] + "; "+ k.coin[i]);    
+        System.out.println("==============================================");
+        System.out.println("Permainan dimulai:");
+        
+        movement(i);
+        lompat  = in.nextInt();
+
+        while (i < k.boxSize - 1) {
+            movement(i);
+            lompat = in.nextInt();
+            
+            if (lompat == 1) {
+                i += 1;
+            } else if (lompat == 2) {
+                i += 2;
+            } else if (lompat == 3) {
+                i -= 1;
+            } else if (lompat == 4) {
+                i -= 2;
+            } else {
+                System.out.println("Opsi yang anda pilih invalid\nSilahkan coba lagi: ");
+                continue;
+            }
+
+            if (k.coin[i] != 0 && k.mons[i] != 0) {
+                System.out.println("Waduh!\nAnda bertemu monster dan koin sekaligus");
+                System.out.println("Point anda sekarang: " + (k.point += k.coin[i] * k.coinScore) + k.mons[i] * k.monsScore);
+                
+                k.mons[i] = 0;
+                k.coin[i] = 0;
+            }
+
+            if (k.coin[i] != 0) {
+                System.out.println("Wah!\nAnda menemukan " + k.coin[i] + " Coin");
+                System.out.println("Point anda sekarang: " + (k.point += k.coin[i] * k.coinScore));
+                
+                k.coin[i] = 0;
+            }
+            if (k.mons[i] != 0) {
+                System.out.println("Awas!\nAnda bertemu " + k.mons[i] + " Monster");
+                System.out.println("Point anda sekarang: " + (k.point += k.mons[i] * k.monsScore));
+
+                k.mons[i] = 0;
+            }
+
+
         }
 
+        // for (int j = 0; j < k.boxSize; j++) {
+        //     System.out.println(j + ": " + k.mons[j] + "; "+ k.coin[j]);    
+        // }
+
+        in.close();
     }
 }
